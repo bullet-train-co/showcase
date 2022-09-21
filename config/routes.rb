8b235d9b841bf1be3091dcbdac5970049b7fa3e9
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-21 18:35:46 +0200

    Strip %2f formatters in nested routing; leave index to viewtale_root_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :viewtale do
-    resources :pages, only: %i[ index show ]
+    get "pages/*id", to: "pages#show", as: :page
 
     root to: "pages#index"
   end
