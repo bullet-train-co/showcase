ec33e2c1135af6df8f6334fba7de7b615162df92
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-21 13:55:25 +0200

    Start building out component grouping support

--- app/models/viewtale/menu.rb
+++ app/models/viewtale/menu.rb
@@ -5,10 +5,17 @@ class Viewtale::Menu
 
   class Item
     attr_reader :path, :name
+    attr_reader :children
 
     def initialize(path)
       path = path.basename.to_s
       @path, @name = path, path.split(".").first
+
+      @children = []
+    end
+
+    def one?
+      children.empty?
     end
   end
 end
