469cabe3c7f12978d2a52dcb1197e7d1d1b74f50
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-21 12:56:35 +0200

    Start fleshing out the preview DSL and how apps hook into our rendering

--- app/models/viewtale/preview.rb
+++ app/models/viewtale/preview.rb
@@ -5,9 +5,30 @@ class Viewtale::Preview
     end
   end
 
-  attr_reader :name, :path
+  attr_reader :name, :path, :examples
 
   def initialize(name)
     @name, @path = name, "tales/#{name}"
+    @examples = []
+  end
+
+  def title(value = nil)
+    @title = value if value
+    @title
+  end
+
+  def description(value = nil)
+    @description = value if value
+    @description
+  end
+
+  def example(&block)
+    @examples << Example.new(block)
+  end
+
+  class Example < Struct.new(:block)
+    def source
+      # TODO: Auto-derive source for an example by evaluating the code with a different output buffer
+    end
   end
 end
