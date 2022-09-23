4abc9e25f7ddfd2d394846b4e66218ed8f0d26d7
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-23 19:28:26 +0200

    Don't find all displays when doing individual lookup

--- app/models/showcase/displaycase.rb
+++ app/models/showcase/displaycase.rb
@@ -4,14 +4,13 @@ class Showcase::Displaycase
   mattr_reader :root, default: Rails.root.join("app/views/showcases")
 
   def self.all
-    Dir.glob("*", base: root).map { new _1 }.sort_by(&:name)
+    Dir.glob("*", base: root).map { new(_1).tap(&:find_displays) }.sort_by(&:name)
   end
 
   attr_reader :name, :displays
 
   def initialize(name)
     @name, @displays = name, []
-    find_displays
   end
   delegate :each, :<<, to: :displays
 
@@ -19,9 +18,7 @@ class Showcase::Displaycase
     Showcase::Display.new(id).tap { _1.group = self }
   end
 
-  private
-
-  def find_displays
+  protected def find_displays
     Dir.children(root.join(name)).map { self << find(_1) }
     displays.sort_by!(&:name)
   end
