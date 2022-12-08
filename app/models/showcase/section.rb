d026a10aa83a35b46f621c90dc5b46eceeea7898
Kasper Timm Hansen <kaspth@gmail.com>
2022-12-08 20:53:27 +0100

    Hotfix the root render

--- app/models/showcase/section.rb
+++ app/models/showcase/section.rb
@@ -3,23 +3,23 @@ class Showcase::Section
 
   mattr_reader :root, default: Rails.root.join("app/views/showcases")
 
-  def self.all
-    Dir.glob("*", base: root).map { new(_1).tap(&:find_displays) }.sort_by(&:name)
+  def self.all(view_context)
+    Dir.glob("*", base: root).map { new(view_context, _1).tap(&:find_displays) }.sort_by(&:name)
   end
 
   attr_reader :name, :displays
 
-  def initialize(name)
-    @name, @displays = name, []
+  def initialize(view_context, name)
+    @view_context, @name, @displays = view_context, name, []
   end
   delegate :each, :<<, to: :displays
 
   def find(id)
-    Showcase::Display.new(self, id)
+    Showcase::Display.new(@view_context, title: id)
   end
 
   protected def find_displays
     Dir.children(root.join(name)).map { self << find(_1) }
-    displays.sort_by!(&:name)
+    displays.sort_by!(&:title)
   end
 end
