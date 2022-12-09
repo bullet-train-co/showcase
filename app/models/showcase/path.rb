cf6874237dfe2b91233fde9c0d89983195a1e015
Kasper Timm Hansen <kaspth@gmail.com>
2022-12-09 18:56:18 +0100

    Replace Section with Tree and Path objects, hopefully clearer

---
+++ app/models/showcase/path.rb
@@ -0,0 +1,18 @@
+class Showcase::Path
+  class Tree < Struct.new(:id, :paths)
+    def self.all
+      Showcase::Path.all.group_by(&:dirname).map { new _1, _2 }
+    end
+  end
+
+  def self.all
+    Showcase.filenames.map { new _1 }.sort_by!(&:id)
+  end
+
+  attr_reader :id, :dirname, :basename
+
+  def initialize(path)
+    @id = path.split(".").first
+    @dirname, @basename = File.split(@id)
+  end
+end
--- lib/showcase.rb
+++ lib/showcase.rb
@@ -2,6 +2,11 @@ require "showcase/version"
 require "showcase/engine"
 
 module Showcase
-  singleton_class.attr_accessor :sample_renderer
+  singleton_class.attr_accessor :root, :sample_renderer
+  @root = "app/views/showcase"
   @sample_renderer = ->(lines) { lines.join }
+
+  def self.filenames
+    Dir.glob("**/*.*", base: root)
+  end
 end
