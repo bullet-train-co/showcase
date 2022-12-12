7881947a13b062b0676306bab158e712a3ac275c
Kasper Timm Hansen <kaspth@gmail.com>
2022-12-12 18:49:05 +0100

    Support nesting the lookups in a directory prefix

--- lib/showcase.rb
+++ lib/showcase.rb
@@ -2,11 +2,15 @@ require "showcase/version"
 require "showcase/engine"
 
 module Showcase
-  singleton_class.attr_accessor :root, :sample_renderer
-  @root = "showcase/pages/templates"
+  singleton_class.attr_accessor :templates_directory_prefix, :sample_renderer
+  @templates_directory_prefix = ""
   @sample_renderer = ->(lines) { lines.join }
 
+  def self.templates_path
+    File.join(templates_directory_prefix, "showcase/pages/templates").delete_prefix("/")
+  end
+
   def self.filenames
-    Dir.glob("**/*.*", base: File.join("app/views", root))
+    Dir.glob("**/*.*", base: File.join("app/views", templates_path))
   end
 end
