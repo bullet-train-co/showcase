44c573e27cf811f9b42dc736e195f91823ff9874
Kasper Timm Hansen <kaspth@gmail.com>
2022-12-23 13:21:20 +0100

    Add a default redirect so test/dummy can be a sample app

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: redirect("/docs/showcase")
 
   mount Showcase::Engine, at: "docs/showcase"
 end
