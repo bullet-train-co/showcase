5c567c928d4bf557897d49fb63ed849ff91e8d8a
Kasper Timm Hansen <kaspth@gmail.com>
2023-01-05 18:16:26 +0100

    Inject application helpers to help rendering of samples

--- app/controllers/showcase/application_controller.rb
+++ app/controllers/showcase/application_controller.rb
@@ -1,3 +1,7 @@
 class Showcase::ApplicationController < ActionController::Base
   layout "showcase"
+
+  if defined?(::ApplicationController)
+    helper all_helpers_from_path ::ApplicationController.helpers_path
+  end
 end
