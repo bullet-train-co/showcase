568e3e68e9db47c654addda84c8041ba0116e3e6
Sean Doyle <sean.p.doyle24@gmail.com>
2023-02-03 10:14:16 -0500

    Add HTML Test Coverage
    Add HTML-level test coverage at both the Integration Test and View
    Partial test layers.
    
    To improve that coverage, depend on [capybara_accessible_selectors][]
    and [action_dispatch-testing-integration-capybara][] to provide access
    to Capybara's HTML parsing and semantic assertions.
    
    In some cases, the [capybara_accessible_selectors][]-powered semantic
    assertions uncovered some inaccessible regions, so this commit also
    ensures that `<section>` element are labelled by corresponding `<h#>`
    elements.
    
    [capybara_accessible_selectors]: https://github.com/citizensadvice/capybara_accessible_selectors/tree/v0.10.0
    [action_dispatch-testing-integration-capybara]: https://github.com/thoughtbot/action_dispatch-testing-integration-capybara/tree/v0.1.1
    
    Co-authored-by: Kasper Timm Hansen <kaspth@gmail.com>

---
+++ test/capybara_extensions.rb
@@ -0,0 +1,5 @@
+module CapybaraExtensions
+end
+
+require "capybara_extensions/assertions"
+require "capybara_extensions/filters/data"
---
+++ test/views/showcase/pages/_sample.html.erb_test.rb
@@ -0,0 +1,49 @@
+require "test_helper"
+
+module Showcase::Pages
+  class SamplePartialTest < ActionView::TestCase
+    test "showcase/pages/sample renders its name and description" do
+      sample = showcase_sample "A sample" do |partial|
+        partial.description { "A description" }
+      end
+
+      render "showcase/pages/sample", sample: sample
+
+      assert_region "A sample" do |section|
+        section.assert_element "showcase-sample" do |showcase_sample|
+          showcase_sample.assert_link "A sample", href: "#a-sample"
+          showcase_sample.assert_text "A description"
+        end
+      end
+    end
+
+    test "showcase/pages/sample renders a preview and its source" do
+      sample = showcase_sample { "<pre>ERB</pre>" }
+
+      render "showcase/pages/sample", sample: sample
+
+      assert_element "showcase-sample" do |showcase_sample|
+        showcase_sample.assert_text "ERB"
+        showcase_sample.assert_disclosure "View Source", expanded: false
+      end
+    end
+
+    test "showcase/pages/sample renders a region to capture JavaScript events" do
+      sample = showcase_sample("with events", events: "click") { "<pre>ERB</pre>" }
+
+      render "showcase/pages/sample", sample: sample
+
+      assert_element "showcase-sample", events: ["click"] do |showcase_sample|
+        showcase_sample.assert_region "JavaScript Events" do |section|
+          section.assert_element data: {showcase_sample_target: "relay"}
+        end
+      end
+    end
+
+    def showcase_sample(name = "sample name", ...)
+      page = Showcase::Page.new(view, id: "showcase_test")
+
+      page.sample(name, ...).first
+    end
+  end
+end
