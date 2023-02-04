9568e0bfee92d9c4677a881a07f0f4f5204e2121
Kasper Timm Hansen <kaspth@gmail.com>
2023-02-04 23:58:04 +0100

    Move previews controller lookup to within showcase/engine/previews; update controller tests

--- app/controllers/showcase/previews_controller.rb
+++ app/controllers/showcase/previews_controller.rb
@@ -1,4 +1,8 @@
 class Showcase::PreviewsController < Showcase::EngineController
+  # Inserts engine into Rails' controller view lookup.
+  # Ref: https://github.com/rails/rails/blob/4b560ab00c1de5ab763b1a0fbfad4d1ea8b60fe7/actionview/lib/action_view/view_paths.rb#L68
+  class_attribute :local_prefixes, default: ["showcase/engine/previews"]
+
   def index
   end
 
