6692a8a79fc65c0aa3c4fad3a9e465c75ef8bb5b
Kasper Timm Hansen <kaspth@gmail.com>
2023-02-04 04:05:51 +0100

    Move version test into our new ShowcaseTest, so we don't clash

--- test/integration/showcase_test.rb
+++ test/integration/showcase_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class ShowcaseTest < Showcase::IntegrationTest
+  test "it has a version number" do
+    assert Showcase::VERSION
+  end
+
   def assert_showcase_preview(path)
   end
 end
