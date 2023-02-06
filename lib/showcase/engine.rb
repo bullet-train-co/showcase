8c7be440355bf2ab27ce99a71da4c9579323ddbd
Kasper Timm Hansen <kaspth@gmail.com>
2023-02-07 00:16:03 +0100

    Add automatic integration testing via autorun (#16)
    This mirrors `Minitest.autorun` by installing an `at_exit` hook,
    which will define the tests on `Showcase::IntegrationTest` directly
    in case there's no defined subclasses.

--- lib/showcase/engine.rb
+++ lib/showcase/engine.rb
@@ -2,8 +2,12 @@ module Showcase
   class Engine < ::Rails::Engine
     isolate_namespace Showcase
 
-    initializer "showcase.assets" do |app|
-      app.config.assets.precompile += %w[showcase_manifest]
+    initializer "showcase.assets" do
+      config.assets.precompile += %w[showcase_manifest]
+    end
+
+    initializer "showcase.integration_test.autorun" do
+      Showcase::IntegrationTest.autorun if Rails.env.test?
     end
   end
 end
