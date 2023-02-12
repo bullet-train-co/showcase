d8ad54fa16f5dbf9a73048030530436e3a459b08
Kasper Timm Hansen <kaspth@gmail.com>
2023-02-12 19:33:44 +0100

    No need for this anymore after new API

--- app/controllers/showcase/previews_controller.rb
+++ app/controllers/showcase/previews_controller.rb
@@ -1,7 +1,4 @@
 class Showcase::PreviewsController < Showcase::EngineController
-  singleton_class.attr_accessor :view_context
-  after_action { self.class.view_context = view_context } if Rails.env.test?
-
   def show
     @preview = Showcase::Path.new(params[:id]).preview_for view_context
   end
