222d45170169eb8b56be453bdde69ac04c075d04
Kasper Timm Hansen <kaspth@gmail.com>
2023-03-28 20:02:46 +0200

    Trim reset styles (#62)
    * Remove forms plugin; we don't want to override app styles
    
    * Remove remaining unused plugins
    
    * Move preflight to application and disable it, so we can start trimming away at it
    
    * Trim styles we don't need
    
    * Trim more resets we don't use
    
    * Don't use any of these
    
    * Move list padding reset to the one ul we use
    
    * Trim more
    
    * Extract .sc-link to remove resets on a tags
    
    * Trim more styles
    
    * Trim more styles
    
    * Extract dummy-button styles to remove more resets
    
    * Not really our place to define for apps
    
    * Trim more needless styles
    
    * Trim
    
    * Consolidate some styles
    
    * Move out list-item style
    
    * Expand link so the whole surface is clickable
    
    * Remove default border styles
    
    * Trim away heading styles
    
    * Use divs for these so we don't hit default margin
    
    * Tweaks on index page
    
    * Don't need to tweak this
    
    * Graduate the slimmer styles to a tailwind input file

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,9 +15,8 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: []
 }
